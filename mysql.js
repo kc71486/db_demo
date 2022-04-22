b8bbfcb3fd28933fa042c6bd15a5517b23e6493b
kc71486 <linda20030102@gmail.com>
2022-04-22 11:27:26 +0800

    add step 4&5

--- mysql.js
+++ mysql.js
@@ -17,11 +17,12 @@ connection.query('CREATE TABLE IF NOT EXISTS student (id VARCHAR(10), name VARCH
 connection.query('CREATE TABLE IF NOT EXISTS course (id VARCHAR(10), name VARCHAR(30))')
 connection.query('CREATE TABLE IF NOT EXISTS score (id VARCHAR(10), score VARCHAR(10))')
 
-connection.query('show TABLES', function (error, results, fields) {
+connection.query('SHOW TABLES', function (error, results, fields) {
   if (error) throw error
   console.log('There are tables: ', results)
 })
 // Step 3 code goes here
+/*
 connection.query('INSERT INTO student (id, name, cid) VALUES ("E24086234", "kc71486", "W0001"), ("A12345679", "StudentB", "M0001")', (err, result) => {
   if (err) console.log('fail to insert:', err)
 })
@@ -31,8 +32,19 @@ connection.query('INSERT INTO course (id, name) VALUES ("W0001", "Web Programmin
 connection.query('INSERT INTO score (id, score) VALUES ("E24086234", 80), ("A123456789", 90)', (err, result) => {
   if (err) console.log('fail to insert:', err)
 })
+*/
 // Step 4 code goes here
-
+connection.query('SELECT id FROM student WHERE name LIKE E24086234', function (error, results, fields) {
+  if (error) throw error
+  console.log('My id is: ', results)
+})
 // Step 5 code goes here
-
+const sql = `
+  SELECT student.id, student.name FROM student
+  JOIN course ON course.id = student.cid
+  WHERE course.name LIKE 'Web Programming'`
+connection.query(sql, (err, rows, fields) => {
+  if (err) console.log('fail to que	ry: ', err)
+  else console.log(rows)
+})
 connection.end()
